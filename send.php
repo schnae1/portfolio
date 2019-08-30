16aaa53dd4e8e7ca6710acd9dc2b3b570448515c
schnae1 <schnaeric@gmail.com>
2019-08-30 11:30:01 -0700

    updates

--- send.php
+++ send.php
@@ -2,8 +2,10 @@
     $to = "eric@ericsportfolio.site";
     $subject = "Email from ericsportfolio.site";
     $name = $_POST['name'];
+    $email = $_POST['email'];
     $message = $_POST['message'];
     $formcontent="From: $name \n Message: $message";
     $mailheader = "From: $email \r\n";
-    mail($to, $subject, $formcontent, $mailheader);
+    mail($to, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
 ?>

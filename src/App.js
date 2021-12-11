0146e6aee85059d5a47a114ff2e099182a967b2a
schnae1 <38385499+schnae1@users.noreply.github.com>
2021-12-10 19:35:48 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/Home';
+import { Route, Link, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;

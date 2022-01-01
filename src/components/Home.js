73758de98bf3c1f20bc891897df893e567c46489
schnae1 <38385499+schnae1@users.noreply.github.com>
2021-12-31 19:13:35 -0800

    Adding typewritter effect to home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/home.css';
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
+  useEffect(() => {
+    console.log("Worked...");
+    // Figure out logic to start typewriter effect here??
+  }, []);
   return (
     <div className='home'>
-      <h1>Home</h1>
+      <div className='typewriter-container'>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.pauseFor(2500)
+            .typeString("Hello,")
+            .pauseFor(1500)
+            .typeString(" I'm <span style=\"color: #e00555;\">Eric</span>.")
+            .pauseFor(1000)
+            .typeString(" I'm a software engineer.")
+            .start();
+          }}
+        />
+      </div>
     </div>
   );
 }

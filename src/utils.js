46eceaed327a1d283b5a088ee23a20967f4cc3ae
schnae1 <38385499+schnae1@users.noreply.github.com>
2023-02-25 10:54:11 -0800

    update url

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export function getBaseURL() {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     return 'http://localhost:8080';
   } else {
-    return 'https://ericsportfolio.site';
+    return 'https://secure-falls-49267.herokuapp.com';
   }
 }
 
